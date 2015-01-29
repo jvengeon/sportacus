83ff3559bd1a1fde66de8f5eb2c3eef02e379bb0
Julien Vengeon <j.vengeo@prismamedia.com>
2015-01-29 16:47:38 +0100

    create homepage

--- src/Sportacus/CoreBundle/Controller/HomeController.php
+++ src/Sportacus/CoreBundle/Controller/HomeController.php
@@ -4,10 +4,10 @@ namespace Sportacus\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class HomeController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('SportacusCoreBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('SportacusCoreBundle:Home:index.html.twig', array());
     }
 }
